45cd9448b78bc80a643044310dbbdd079777d1d7
Grayson Wright <wright.grayson@gmail.com>
2015-08-24 17:14:47 -0700

    Add authorization logic for admin dashboards
    Admin github handles can be set through an environment variable.

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,10 +7,6 @@
 class Admin::ApplicationController < Administrate::ApplicationController
   before_filter :authenticate_admin
 
-  def authenticate_admin
-    # TODO Add authentication logic here.
-  end
-
   def index
     super
 
@@ -19,5 +15,22 @@ class Admin::ApplicationController < Administrate::ApplicationController
       Customize this action to update/remove the limit,
       or implement the pagination library of your choice."
     @resources = @resources.limit(20)
+  end
+
+  private
+
+  def authenticate_admin
+    unless current_user &&
+      admin_github_handles.include?(current_user.github_username)
+      redirect_to :root
+    end
+  end
+
+  def current_user
+    @current_user ||= User.where(remember_token: session[:remember_token]).first
+  end
+
+  def admin_github_handles
+    ENV.fetch("ADMIN_GITHUB_HANDLES", "").split(",")
   end
 end
---
+++ spec/features/admin_authorization_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+feature "Admin authorization" do
+  scenario "admin accesses dashboard" do
+    token = "usergithubtoken"
+    user = create(:user, github_username: "admin_user")
+    stub_repos_requests(token)
+    allow(ENV).to receive(:fetch).
+      with("ADMIN_GITHUB_HANDLES").
+      and_return("admin_user,other_admin_user")
+
+    sign_in_as(user, token)
+    visit "/admin"
+
+    expect(page).to have_content "Administrate"
+  end
+
+  scenario "non-admin cannot access dashboard" do
+    token = "usergithubtoken"
+    user = create(:user, github_username: "not_admin_user")
+    stub_repos_requests(token)
+    allow(ENV).to receive(:fetch).
+      with("ADMIN_GITHUB_HANDLES").
+      and_return("admin_user,other_admin_user")
+
+    sign_in_as(user, token)
+    visit "/admin"
+
+    expect(page).not_to have_content "Administrate"
+  end
+end
