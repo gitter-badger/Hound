ef341f97c231bd14cd60c69ab846f3b22b73fe38
Grayson Wright <wright.grayson@gmail.com>
2015-08-25 10:52:25 -0700

    Fix PR suggestions

--- spec/features/admin_authorization_spec.rb
+++ spec/features/admin_authorization_spec.rb
@@ -2,28 +2,22 @@ require "rails_helper"
 
 feature "Admin authorization" do
   scenario "admin accesses dashboard" do
-    token = "usergithubtoken"
-    user = create(:user, github_username: "admin_user")
+    ENV["ADMIN_GITHUB_HANDLES"] = "admin_user,other_admin_user"
     stub_repos_requests(token)
-    allow(ENV).to receive(:fetch).
-      with("ADMIN_GITHUB_HANDLES", "").
-      and_return("admin_user,other_admin_user")
+    admin = create(:user, github_username: "admin_user")
 
-    sign_in_as(user, token)
+    sign_in_as(admin, token)
     visit admin_users_path
 
     expect(page).to have_admin_users_header
   end
 
   scenario "non-admin cannot access dashboard" do
-    token = "usergithubtoken"
-    user = create(:user, github_username: "not_admin_user")
+    ENV["ADMIN_GITHUB_HANDLES"] = "admin_user,other_admin_user"
     stub_repos_requests(token)
-    allow(ENV).to receive(:fetch).
-      with("ADMIN_GITHUB_HANDLES", "").
-      and_return("admin_user,other_admin_user")
+    non_admin = create(:user, github_username: "not_admin_user")
 
-    sign_in_as(user, token)
+    sign_in_as(non_admin, token)
     visit admin_users_path
 
     expect(page).not_to have_admin_users_header
@@ -31,5 +25,9 @@ feature "Admin authorization" do
 
   def have_admin_users_header
     have_selector("h1", text: "Users")
+  end
+
+  def token
+    "usergithubtoken"
   end
 end
